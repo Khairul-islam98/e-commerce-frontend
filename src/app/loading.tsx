687682fb2ee0bcff1661c252d8406261d8e14a56
khairul-islam98 <janeyrahman900@gmail.com>
2024-12-04 00:39:26 +0600

    add loading spinner component

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+
+export default function Loading() {
+    
+    return (
+        <div className='h-full flex items-center justify-center'>
+         <div className="h-8 w-8 border-4 border-t-rose-600 animate-spin rounded-full"></div>
+    </div>
+    );
+  }
