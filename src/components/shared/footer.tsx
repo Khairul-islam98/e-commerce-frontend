83ab0c50b7cb4de3469ac184b863901cbdf26aa6
khairul-islam98 <janeyrahman900@gmail.com>
2024-12-04 17:49:53 +0600

    add footer component

---
+++ src/components/shared/footer.tsx
@@ -0,0 +1,14 @@
+
+
+export const Footer = () => {
+    return (
+        <footer className="">
+        <div className="container mx-auto text-center">
+          <p className="text-gray-400 text-sm">
+            © 2024 ShopHub. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    );
+};
+
