adaf4a40dd58c3af13540a3ed3a41d3f06b6c665
khairul-islam98 <janeyrahman900@gmail.com>
2025-01-19 22:37:57 +0600

    Add root layout component with custom fonts and metadata for Shop Hub

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Providers from "@/providers";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Shop Hub",
+  description: "Shop Hub - Your one-stop solution for all your shopping needs.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-[#313338]`}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
