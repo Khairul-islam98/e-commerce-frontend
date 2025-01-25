628ace1909a3815f210ac14fd1dafadf96480142
khairul-islam98 <janeyrahman900@gmail.com>
2025-01-26 00:03:28 +0600

    Add CartPage component to display cart items

---
+++ src/app/(rootLayout)/cart/page.tsx
@@ -0,0 +1,12 @@
+import { CartItem } from '@/components/cart/cart-item';
+import React from 'react';
+
+const CartPage = () => {
+    return (
+        <>
+        <CartItem />
+        </>
+    );
+};
+
+export default CartPage;
