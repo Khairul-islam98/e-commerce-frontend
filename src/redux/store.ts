792257b8cfc8aba823433491e997f8dfd8d6f7a5
khairul-islam98 <janeyrahman900@gmail.com>
2025-03-03 21:21:28 +0600

    Add Redux store configuration with persistence for auth and cart reducers

---
+++ src/redux/store.ts
@@ -0,0 +1,48 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./api/baseApi";
+import authReducer from "./features/auth/authSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import cartReducer from "./features/cart/cartSlice";
+import checkoutReducer from "./features/checkout/chekoutSlice";
+import productComparisonReducer from "./features/product/productSlice";
+
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+const persistedCartReducer = persistReducer(
+  { ...persistConfig, key: "cart" },
+  cartReducer
+);
+
+export const store = configureStore({
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: persistedAuthReducer,
+    cart: persistedCartReducer,
+    checkout: checkoutReducer,
+    productComparison: productComparisonReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
