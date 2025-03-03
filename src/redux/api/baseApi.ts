469062cfc38e1b090298b29364f4990297a4c8ef
khairul-islam98 <janeyrahman900@gmail.com>
2025-03-04 00:32:11 +0600

    Add base API configuration with token refresh handling for Redux Toolkit Query

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,74 @@
+import envConfig from "@/config/env-config";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { logout, setUser } from "../features/auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: envConfig.baseApi,
+  // credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 401) {
+    //* Send Refresh
+
+    const res = await fetch(`${envConfig.baseApi}/auth/refreshToken`, {
+      method: "POST",
+      // credentials: "include",
+    });
+
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        })
+      );
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+
+  return result;
+};
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: baseQueryWithRefreshToken,
+  tagTypes: [
+    "user",
+    "product",
+    "category",
+    "review",
+    "shop",
+    "order",
+    "coupon",
+    "payment",
+  ],
+  endpoints: () => ({}),
+});
